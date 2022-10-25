165ea9e411b39385503e5e928b0d360d112892e5
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2022-10-25 06:13:19 -0400

    Redirect to event rather than home

--- www/upload-done.js
+++ www/upload-done.js
@@ -1,9 +1,11 @@
+var redirect = "/"
 if (location.hash){
     var keys = location.hash.replace(/^\#/,"").split(/,/)
     for (var i=0; i<keys.length; i++){
         localStorage.removeItem(keys[i])
+        redirect = "/event.html#"+keys[i].replace(/_.*/,"")
     }
 }
 setTimeout(function(){
-    location.href = "/"
-},5000)
+    location.href = redirect
+},3000)
