bbc00f0a02a1a2735ff3d05bdfca19eaff408655
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2022-10-27 05:55:29 -0400

    Add year drop down to front page

--- www/index.js
+++ www/index.js
@@ -1,20 +1,45 @@
 $(document).ready(function(){
+    var events = []
     $.ajax({
         url:"/event-list.cgi",
         dataType:"text",
         success:function(data){
-            var events = data.split(/[\r\n]/)
-            var list = $('#events-list')
-            var filter = location.hash.replace(/^\#/,"")
-            console.log(filter)
+            events = data.split(/[\r\n]/)
+            showEvents()
+            var years = {}
             for (var i=0; i<events.length; i++){
-                if (events[i] && events[i].startsWith(filter)){
-                    var event = events[i]
-                    var year = event.substring(0,4)
-                    var venue = event.substring(4)
-                    list.append($(`<li><a href=/event.html#${event}>${year} ${venue}</a></li>`))
-                }
+                var m = events[i].match(/^[0-9]{4}/)
+                if(m) years[m[0]] = 1
             }
+            years = Object.keys(years)
+            years.sort((a,b) => {return a-b})
+            for (var i=0; i<years.length; i++){
+                var year = years[i]
+                $('#years').append($(`<option value=${year}>${year}</option>`))
+            }
+            if (!years.length) $('#years').hide()
+        }
+    })
+    function showEvents(){
+        var list = $('#events-list')
+        list.html('');
+        var filter = location.hash.replace(/^\#/,"")
+        if (!filter) filter = $('#years option:last').attr('value')
+        for (var i=0; i<events.length; i++){
+            if (events[i] && events[i].startsWith(filter)){
+                var event = events[i]
+                var year = event.substring(0,4)
+                var venue = event.substring(4)
+                list.append($(`<li><a href=/event.html#${event}>${year} ${venue}</a></li>`))
+            }
+        }
+        $('#years').val('-')
+    }
+    $(window).on('hashchange', showEvents)
+    $('#years').change(function(){
+        year = $('#years').val()
+        if (/^[0-9]{4}$/.test(year)){
+            location.hash = `#${year}`
         }
     })
 })
