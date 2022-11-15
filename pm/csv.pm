f80510613821efe1d668414790825941351b9213
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2022-11-15 17:34:39 -0500

    Whitespace

--- pm/csv.pm
+++ pm/csv.pm
@@ -3,50 +3,50 @@
 package csv;
 
 sub new {
-   my ($class, $data) = @_;
-   $data = [ map { [ split(/,/, $_, -1) ] } split(/[\r\n]+/, $data) ];
-   my $headers = shift @{$data};
-   $headerMap = { map { $headers->[$_] => $_ } 0..(scalar(@{$headers})-1) };
-   my $self = {
-      _data => $data,
-      _headers => $headers,
-      _headerMap => $headerMap,
-   };
-   bless $self, $class;
-   return $self;
+	my ($class, $data) = @_;
+	$data = [ map { [ split(/,/, $_, -1) ] } split(/[\r\n]+/, $data) ];
+	my $headers = shift @{$data};
+	$headerMap = { map { $headers->[$_] => $_ } 0..(scalar(@{$headers})-1) };
+	my $self = {
+		_data => $data,
+		_headers => $headers,
+		_headerMap => $headerMap,
+	};
+	bless $self, $class;
+	return $self;
 }
 
 sub getRowCount(){
-   my( $self ) = @_;
-   return scalar(@{$self->{_data}});
+	my( $self ) = @_;
+	return scalar(@{$self->{_data}});
 }
 
 sub getItemCount(){
-   my($self, $row) = @_;
-   return 0 if ($row >= $self->getRowCount());
-   return scalar(@{$self->{_data}->[$row]});
+	my($self, $row) = @_;
+	return 0 if ($row >= $self->getRowCount());
+	return scalar(@{$self->{_data}->[$row]});
 }
 
 sub getHeaderCount(){
-   my( $self ) = @_;
-   return scalar(@{$self->{_headers}})-1;
+	my( $self ) = @_;
+	return scalar(@{$self->{_headers}})-1;
 }
 
 sub getValue(){
-   my($self, $row, $col) = @_;
-   return $self->{_headers}->[$col] if ($row == 0);
-   return $self->{_data}->[$row-1]->[$col];
+	my($self, $row, $col) = @_;
+	return $self->{_headers}->[$col] if ($row == 0);
+	return $self->{_data}->[$row-1]->[$col];
 }
 
 sub getByName(){
-   my($self, $row, $colName) = @_;
-   return $self->{_headers}->[$self->{_headerMap}->{$colName}] if ($row == 0);
-   return $self->{_data}->[$row-1]->[$self->{_headerMap}->{$colName}];
+	my($self, $row, $colName) = @_;
+	return $self->{_headers}->[$self->{_headerMap}->{$colName}] if ($row == 0);
+	return $self->{_data}->[$row-1]->[$self->{_headerMap}->{$colName}];
 }
 
 sub getData() {
-   my( $self ) = @_;
-   return $self->{_data};
+	my( $self ) = @_;
+	return $self->{_data};
 }
 
 1;
--- pm/webutil.pm
+++ pm/webutil.pm
@@ -5,34 +5,34 @@ package webutil;
 use HTML::Entities;
 
 sub new {
-   my ($class) = @_;
-   my $self = {};
-   bless $self, $class;
-   return $self;
+	my ($class) = @_;
+	my $self = {};
+	bless $self, $class;
+	return $self;
 }
 
 sub error {
-    my ($self, $title, $help) = @_;
-    print "Content-type: text/html; charset=UTF-8\n\n";
-    print "<!DOCTYPE html>\n";
-    print "<html>\n";
-    print "<head>\n";
-    print "<meta charset=UTF-8>\n";
-    print "<title>".encode_entities($title)."</title>\n";
-    print "</head>\n";
-    print "<body>\n";
-    print "<h1 style=color:red>".encode_entities($title)."</h1>\n";
-    print "<pre>".encode_entities($help||"")."</pre>\n";
-    print "</body>\n";
-    print "</html>\n";
-    exit 0;
+	 my ($self, $title, $help) = @_;
+	 print "Content-type: text/html; charset=UTF-8\n\n";
+	 print "<!DOCTYPE html>\n";
+	 print "<html>\n";
+	 print "<head>\n";
+	 print "<meta charset=UTF-8>\n";
+	 print "<title>".encode_entities($title)."</title>\n";
+	 print "</head>\n";
+	 print "<body>\n";
+	 print "<h1 style=color:red>".encode_entities($title)."</h1>\n";
+	 print "<pre>".encode_entities($help||"")."</pre>\n";
+	 print "</body>\n";
+	 print "</html>\n";
+	 exit 0;
 }
 
 sub redirect {
-    my ($self, $url) = @_;
-    $url = "http".($ENV{'HTTPS'}?"s":"")."://".$ENV{'SERVER_NAME'}.($ENV{'SERVER_PORT'}=~/^80|443$/?"":":".$ENV{'SERVER_PORT'}).$url if ($url =~ /^\//);
-    print "Location: $url\n\n";
-    exit 0;
+	 my ($self, $url) = @_;
+	 $url = "http".($ENV{'HTTPS'}?"s":"")."://".$ENV{'SERVER_NAME'}.($ENV{'SERVER_PORT'}=~/^80|443$/?"":":".$ENV{'SERVER_PORT'}).$url if ($url =~ /^\//);
+	 print "Location: $url\n\n";
+	 exit 0;
 }
 
 1;
