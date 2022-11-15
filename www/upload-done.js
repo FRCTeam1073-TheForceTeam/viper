8619e4d7e6b7a841c8274047d322ff8f5d2930e4
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2022-11-15 17:00:08 -0500

    Key to variable

--- www/upload-done.js
+++ www/upload-done.js
@@ -2,8 +2,11 @@ var redirect = "/"
 if (location.hash){
     var keys = location.hash.replace(/^\#/,"").split(/,/)
     for (var i=0; i<keys.length; i++){
-        localStorage.removeItem(keys[i])
-        redirect = "/event.html#"+keys[i].replace(/_.*/,"")
+        var key = keys[i]
+        if (/^20\d\d/.test(key)){
+            localStorage.removeItem(key)
+            redirect = "/event.html#"+key.replace(/_.*/,"")
+        }
     }
 }
 setTimeout(function(){
