a1be2036addf1bc3eb0e642a2b3110b6fda33624
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2022-12-05 05:35:36 -0500

    Adjust caching

--- www/offline-service-worker.cgi
+++ www/offline-service-worker.cgi
@@ -2,12 +2,18 @@
 
 print "Content-type: text/javascript;charset=UTF-8\n\n";
 
+my $tmpHome = `mktemp -d /tmp/XXXXXXXXXX`;
+chomp $tmpHome;
+my $gitdir = `readlink  -f ..`;
+`HOME=$tmpHome git config --global --add safe.directory $gitdir`;
+
 if ($ENV{REMOTE_ADDR} eq "127.0.0.1" or $ENV{DEBUG}){
 	# For local development, don't use a service worker cache
 	print "const CACHE_NAME = ''\n\n";
 } else {
-	my $hash = `git rev-parse HEAD`;
+	my $hash = `HOME=$tmpHome git rev-parse HEAD`;
 	chomp $hash;
-	print "const CACHE_NAME = '$hash $ENV{DEBUG}'\n\n";
+	print "const CACHE_NAME = '$hash'\n\n";
 }
 print `cat offline-service-worker.js`;
+`rm -rf $tmpHome`;
--- www/offline-service-worker.js
+++ www/offline-service-worker.js
@@ -5,65 +5,43 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
 	// delete old caches
 	caches.keys().then(function(names) {
-		for (let name of names)
-			if (names != CACHE_NAME) caches.delete(name)
-	});
+		for (let name of names) if (names != CACHE_NAME) caches.delete(name)
+	})
 })
 
 self.addEventListener('fetch', (event) => {
-	//console.log(event.request.url)
-	if (!CACHE_NAME){
-		// Not configured with a cache
-		event.respondWith(fetch(event.request))
-	} else if (/\/(scout|admin)\//.test(event.request.url)){
-		// Uploading data, these request
-		// have to go through to the server
-		// Network only policy
-		//console.log('From network (scout, admin): ' + event.request.url)
-		event.respondWith(fetch(event.request))
-	} else if (/\/20.*\.cgi/.test(event.request.url)){
-		// Legacy CGI
-		//console.log('From network (legacy cgi): ' + event.request.url)
-		event.respondWith(fetch(event.request))
-	} else if (/\/heartbeat\.html/.test(event.request.url)){
-		// Heartbeat connection test
-		//console.log('From network (legacy cgi): ' + event.request.url)
-		event.respondWith(fetch(event.request))
-	} else if (/\.(cgi|csv)/.test(event.request.url)){
+	if (
+		!CACHE_NAME // Cache not configured
+		|| /\.cgi/.test(event.request.url) // Uploads and dynamic data
+		|| /\/heartbeat\.html/.test(event.request.url) // Connection test
+		|| /\/data\//.test(event.request.url) // Uploaded data
+	){
+		// Network first, from cache if network failed
 		event.respondWith(
-			// network first
-			fetch(event.request).then((networkResponse) => {
-				return caches.open(CACHE_NAME).then((cache) => {
-					// to cache after fetching from network
-					//console.log('From network to cache (data): ' + event.request.url)
-					cache.put(event.request, networkResponse.clone())
-					return networkResponse
-				})
-			}).catch(() => {
-				//console.log('From cache (data): ' + event.request.url)
-				// from cache if network failed
+			fetch(event.request).then(putCache).catch(() => {
 				return caches.match(event.request)
 			})
 		)
 	} else {
-		// Everything else is static files
 		// Cache first, but network if it isn't available
 		event.respondWith(
 			caches.match(event.request).then((cacheResponse) => {
 				// If it is available in cache
 				if (cacheResponse){
-					//console.log('From cache (static): ' + event.request.url)
 					return cacheResponse
 				}
 				// If it needs to be fetched and cached
-				return fetch(event.request).then((networkResponse) => {
-					//console.log('From network to cache (static): ' + event.request.url)
-					return caches.open(CACHE_NAME).then((cache) => {
-						cache.put(event.request, networkResponse.clone())
-						return networkResponse
-					})
-				})
+				return fetch(event.request).then(putCache)
 			})
 		)
+	}
+
+	function putCache(networkResponse){
+		// Only put successful responses into cache
+		if (networkResponse.status != 200) return networkResponse
+		return caches.open(CACHE_NAME).then((cache) => {
+			cache.put(event.request, networkResponse.clone())
+			return networkResponse
+		})
 	}
 })
