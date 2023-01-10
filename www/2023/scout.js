62ff19f9fa425cae65a5b7f6c604b4905a1e8831
Oli <24oliviao@gmail.com>
2023-01-10 07:33:51 -0500

    formatting + fixed buttons to return false

--- www/2023/scout.js
+++ www/2023/scout.js
@@ -3,9 +3,11 @@ $(document).ready(function(){
     $('#to-teleop').click(function(){
         $('#auto').hide()
         $('#teleop').show()
+        return false
     })
     $('#to-auto').click(function(){
         $('#teleop').hide()
         $('#auto').show()
+        return false
     })
 })
