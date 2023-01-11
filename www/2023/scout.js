ea3e08a76bafa8f9d7f429ad3e2c9bfc2e6c93c8
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2023-01-10 20:34:43 -0500

    Full cycle time tracking

--- www/2023/scout.js
+++ www/2023/scout.js
@@ -10,4 +10,48 @@ $(document).ready(function(){
         $('#auto').show()
         return false
     })
+
+    var cycles = []
+    var cycle
+    cycleInterrupt()
+
+    $('.placement').click(function(){
+        cycleStage("placement")
+    })
+
+    $('.collectLoadingZone').click(function(){
+        cycleStage("collect")
+    })
+
+    function cycleStage(place){
+        if (cycle.stage==0 || cycle.lastPlace==place){
+            cycle.stage = 1
+            cycle.startTime = Date.now()
+        } else if (cycle.stage==1){
+            cycle.stage = 2
+        } else {
+            var cycleTime = Math.round((Date.now() - cycle.startTime)/1000)
+            if (cycleTime >= 7){ // Faster than seven seconds is not possible, scouter error.
+                cycles.push(cycleTime)
+                $('input[name="full_cycle_fastest_seconds"]').val(Math.min(...cycles))               
+                $('input[name="full_cycle_average_seconds"]').val(Math.round(cycles.reduce((a,b) => a + b, 0) / cycles.length))
+                $('input[name="full_cycle_count"]').val(cycles.length)
+            }
+            cycle.stage = 1
+            cycle.startTime = Date.now()
+        }
+        cycle.lastPlace = place
+    }
+
+    function cycleInterrupt(){
+        cycle = {
+            startTime: 0,
+            lastPlace: "",
+            stage: 0
+        }
+    }
+
+    $('.count,button,label').click(function(){
+        if (!$(this).is('.placement,.collectLoadingZone')) cycleInterrupt()
+    })
 })
