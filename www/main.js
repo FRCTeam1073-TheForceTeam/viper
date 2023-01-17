cad0bb98fa3251f8d8981b6e5c3e1ad029e9a726
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2023-01-17 18:43:07 -0500

    Add year to name

--- www/main.js
+++ www/main.js
@@ -40,7 +40,7 @@ $(document).ready(function(){
 						recentEventId = recent[0]
 						recentEventYear = recentEventId.replace(/([0-9]{4}).*/,'$1')
 						var venue = recentEventId.replace(/[0-9]{4}(.*)/,'$1')
-						recentEventName = recent[1]||`${recentEventYear} ${venue}`
+						recentEventName = `${recentEventYear} ` + (recent[1]||$venue)
 					}
 				}
 			}).always(populateMainMenu)
