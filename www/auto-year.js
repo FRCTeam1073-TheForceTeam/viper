1ff6f503ce881069bad29aa89985383536084d3e
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2023-01-30 06:58:56 -0500

    Add "use strict"

--- www/auto-year.js
+++ www/auto-year.js
@@ -1,3 +1,5 @@
+"use strict"
+
 $(document).ready(function(){
 	$('input[name="event_year"],input#event_year,input[name="year"],input#year').val(new Date().getFullYear())
 	$('input[type="date"]').val(new Date().toISOString().substring(0,10))
