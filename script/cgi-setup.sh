c5b4209c0a6c49c456985b8ae7849fc262a2e70f
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2023-02-26 20:54:15 -0500

    Check for xampp first

--- script/cgi-setup.sh
+++ script/cgi-setup.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-if [ -e /usr/bin/perl ]
-then
-	PERL=/usr/bin/perl
-elif [ -e /c/xampp/perl/bin/perl.exe ]
+if [ -e /c/xampp/perl/bin/perl.exe ]
 then
 	PERL=C:/xampp/perl/bin/perl.exe
+elif [ -e /usr/bin/perl ]
+then
+	PERL=/usr/bin/perl
 else
 	echo "Could not find perl installation"
 	exit 1
@@ -18,9 +18,9 @@ do
 	dst=${src/cgi/www}
 	dstdir=${dst%/*}
 	mkdir -p "$dstdir"
+	rm -f "$dst"
 	if [ "$PERL" == "/usr/bin/perl" ]
 	then
-		rm -f "$dst"
 		ln "$src" "$dst"
 	else
 		sed -E "s|^#!/usr/bin/perl|#!$PERL|g" "$src" > "$dst"
