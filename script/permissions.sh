166af5c81d66dbcb53711d3af3a27e4b384e1def
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2023-02-27 06:46:58 -0500

    Only run on systems with sudo

--- script/permissions.sh
+++ script/permissions.sh
@@ -2,17 +2,20 @@
 
 set -e
 
-# The web server needs to write to this directory
-sudo chown -R `whoami`:www-data www/data/
-sudo chmod -R g+rw www/data/
-# Set it so that new files in that directory
-# use the same group user and permissions
-# as the directory itself
-find  www/data/ -type d -exec sudo chmod ug+s {} \;
+if which sudo &> /dev/null
+then
+	# The web server needs to write to this directory
+	sudo chown -R `whoami`:www-data www/data/
+	sudo chmod -R g+rw www/data/
+	# Set it so that new files in that directory
+	# use the same group user and permissions
+	# as the directory itself
+	find  www/data/ -type d -exec sudo chmod ug+s {} \;
 
-dir=`pwd`
-while [ "z$dir" != "z" ]
-do
-	sudo chmod a+x "$dir"
-	dir="${dir%/*}"
-done
+	dir=`pwd`
+	while [ "z$dir" != "z" ]
+	do
+		sudo chmod a+x "$dir"
+		dir="${dir%/*}"
+	done
+fi
