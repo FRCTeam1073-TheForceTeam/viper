187d77efefde4f4b35519f3144c5c937e14e7364
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2023-02-28 15:24:14 -0500

    Add local CSS

--- script/local-conf.sh
+++ script/local-conf.sh
@@ -4,11 +4,11 @@ set -e
 
 exitStatus=0
 
-if [ ! -e local.conf ]
+if [ ! -e www/local.css ]
 then
 	echo
-	cp -v ./script/example.conf ./local.conf
-	echo "Created local.conf file"
+	cp -v ./script/example.css ./www/local.css
+	echo "Created www/local.css file"
 	echo "Please edit it and re-run this script"
 	exitStatus=1
 fi
@@ -17,7 +17,16 @@ if [ ! -e www/local.js ]
 then
 	echo
 	cp -v ./script/example.js ./www/local.js
-	echo "Created ./www/local.js file"
+	echo "Created www/local.js file"
+	echo "Please edit it and re-run this script"
+	exitStatus=1
+fi
+
+if [ ! -e local.conf ]
+then
+	echo
+	cp -v ./script/example.conf ./local.conf
+	echo "Created local.conf file"
 	echo "Please edit it and re-run this script"
 	exitStatus=1
 fi
