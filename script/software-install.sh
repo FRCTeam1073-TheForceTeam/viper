09ca88d9dcdfcc024b17fe784bcb4c1327254275
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2023-02-28 04:29:43 -0500

    Comment to update both places

--- script/software-install.sh
+++ script/software-install.sh
@@ -4,7 +4,8 @@ set -e
 
 if which apt &> /dev/null
 then
-	# Install software
+	# When changing this list of installed software
+	# PLEASE ALSO UPDATE the similar list in Dockerfile
 	sudo apt-get install -y \
 		apache2 \
 		apache2-utils \
