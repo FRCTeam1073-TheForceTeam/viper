571b242cb0ab2c608837be8f06d47bbf98720848
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2023-02-28 04:17:29 -0500

    Allow min score to be zero

--- www/2023/aggregate-stats.js
+++ www/2023/aggregate-stats.js
@@ -142,7 +142,7 @@ function aggregateStats(scout, aggregate){
 	aggregate["full_cycle_fastest_seconds"] = (aggregate["full_cycle_fastest_seconds"]||999)>scout["full_cycle_fastest_seconds"]?scout["full_cycle_fastest_seconds"]:(aggregate["full_cycle_fastest_seconds"]||999)
 	if (cycles > 0) aggregate["full_cycle_average_seconds"] = Math.round(cycleSeconds / cycles)
 	aggregate["max_score"] = (aggregate["max_score"]||0)<scout["score"]?scout["score"]:(aggregate["max_score"]||0)
-	aggregate["min_score"] = (aggregate["min_score"]||999)>scout["score"]?scout["score"]:(aggregate["min_score"]||999)
+	aggregate["min_score"] = (aggregate["min_score"]===undefined?999:aggregate["min_score"])>scout["score"]?scout["score"]:(aggregate["min_score"]===undefined?999:aggregate["min_score"])
 }
 
 var statInfo = {
