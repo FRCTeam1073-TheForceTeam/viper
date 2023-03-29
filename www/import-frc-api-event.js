2f64ac6ba358ee63b9c73bfbae87098edefdea9d
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2023-03-29 04:46:26 -0400

    Move common methods to their own file

--- www/import-frc-api-event.js
+++ www/import-frc-api-event.js
@@ -36,34 +36,3 @@ $(document).ready(function(){
 		})
 	})
 })
-
-function randomPracticeSchedule(teams){
-	if (!teams || !teams.length) return ""
-	var match=0,
-	matchTeams=0,
-	csv="Match,R1,R2,R3,B1,B2,B3"
-	teams = shuffleArray([...new Set(teams)])
-	teams.forEach(function(team){
-		if (matchTeams >= 6) matchTeams = 0
-		if (matchTeams == 0){
-			match++
-			csv+="\npm"+match
-		}
-		csv+=","+team
-		matchTeams++
-	})
-	while (matchTeams < 6){
-		teams = shuffleArray(teams)
-		csv+=","+teams[0]
-		matchTeams++
-	}
-	csv+="\n"
-	return csv
-}
-
-// https://stackoverflow.com/a/46545530
-function shuffleArray(array){
-	return array.map(value => ({value, sort: Math.random()}))
-	.sort((a, b) => a.sort - b.sort)
-	.map(({value}) => value)
-}
