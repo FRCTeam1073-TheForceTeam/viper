2b4824d4012e554ad9a819006c364b993ffcd4f3
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2023-03-28 20:38:43 -0400

    Import practice matches from first inspires

--- www/event-upload.js
+++ www/event-upload.js
@@ -5,25 +5,47 @@ $(document).ready(function(){
 	$('input[type=submit]').click(processInput)
 
 	function processInput(){
-		var src=$('#sourceInp').val(),csv="",m
+		var src=$('#sourceInp').val(),csv=""
 		if (!csv) csv=getBlueAllianceMatchSchedule(src)
+		if (!csv) csv=getFirstInpsiresMatchSchedule(src)
 		if (!csv) csv=randomPracticeSchedule(getBlueAllianceTeamList(src))
 		if (!csv) csv=randomPracticeSchedule(getGenericTeamList(src))
 		if (!csv){
 			alert("No data found!")
 			return false
 		}
-		$('#idInp').val((src.match(/event_key=(20[0-9]+[a-z]+)/)||["",$('#idInp').val()])[1])
-		$('#startInp').val((src.match(/itemprop\=\"startDate\" datetime\=\"(20[0-9]{2}-[0-9]{2}-[0-9]{2})/)||["",$('#startInp').val()])[1])
-		$('#endInp').val((src.match(/itemprop\=\"endDate\" datetime\=\"(20[0-9]{2}-[0-9]{2}-[0-9]{2})/)||["",$('#endInp').val()])[1])
-		$('#nameInp').val((src.match(/id\=\"event-name\"\>([^\<]+)\</)||["",$('#nameInp').val()])[1])
-		$('#locationInp').val((src.match(/\<span itemprop\=\"location\"\>(.|[\r\n])*?\<\/span\>/m)||["",$('#locationInp').val()])[1])
 		$('#csvInp').val(csv)
-		if($('#autoFields input:empty').length){
+		var newEventId = src.match(/event_key=(20[0-9]+[a-z]+)/)||src.match(/href\=\"\/(20[0-9]{2}\/[A-Z]+)\"\>Event Info/||["",$('#idInp').val()])[1].replace(/\//g,"").toLowerCase()
+		if (newEventId != eventId){
+			eventInfo=null
+			eventId=newEventId
+			$('#idInp').val(eventId)
+			loadEventInfo(function(){
+				if (srcToField(src)){
+					$('#importData').submit()
+				}
+			})
+			return false
+		}
+		srcToField(src)
+	}
+
+	function srcToField(src){
+		$('#startInp').val((src.match(/itemprop\=\"startDate\" datetime\=\"(20[0-9]{2}-[0-9]{2}-[0-9]{2})/)||["",eventInfo.start||$('#startInp').val()])[1])
+		$('#endInp').val((src.match(/itemprop\=\"endDate\" datetime\=\"(20[0-9]{2}-[0-9]{2}-[0-9]{2})/)||["",eventInfo.end||$('#endInp').val()])[1])
+		$('#nameInp').val((src.match(/id\=\"event-name\"\>([^\<]+)\</)||src.match(/\<span class\=\"hidden-xs hidden-sm align-middle\"\> - ([^\<]+)\</)||["",eventInfo.name||$('#nameInp').val()])[1].replace(/ Event$/,""))
+		$('#locationInp').val((src.match(/\<span itemprop\=\"location\"\>(.|[\r\n])*?\<\/span\>/m)||["",eventInfo.location||$('#locationInp').val()])[1])
+		var empty = false
+		$('#autoFields input:empty').each(function(){
+			if (!$(this).val()) empty = true
+		})
+		if(empty){
 			$('#autoFields').show()
 			$('#csvInp').show()
 			$('#sourceInp').hide()
+			return false
 		}
+		return true
 	}
 
 	function venueNameToId(){
@@ -40,11 +62,30 @@ $(document).ready(function(){
 		return src.match(/\b([0-9]+)\b/g)
 	}
 
+	function getFirstInpsiresMatchSchedule(src){
+		var isPractice = /Official practice match schedule/.test(src),
+		re= /\/20[0-9]{2}\/team\/([0-9]+)/g,
+		m, matchNum=1, match=[], schedule = [["Match","R1","R2","R3","B1","B2","B3"]]
+		while(m = re.exec(src)){
+			if(!match.length){
+				match.push((isPractice?"pm":"qm")+matchNum)
+				matchNum++
+			}
+			match.push(m[1])
+			if (match.length==7){
+				schedule.push(match)
+				match=[]
+			}
+		}
+		if (schedule.length==1) return ""
+		return schedule.map(a=>a.join(",")).join("\n")+"\n"
+	}
+
 	function getBlueAllianceMatchSchedule(src){
 		var re= /(?:\/match\/(20[0-9]{2}[a-zA-Z0-9]+)_qm([0-9]+))|(?:\/team\/([0-9]+)\/20)/g,
 		m, qual, schedule = [["Match","R1","R2","R3","B1","B2","B3"]]
 		do {
-			m = re.exec(src);
+			m = re.exec(src)
 			if (m) {
 				if (m[1]){
 					qual = parseInt(m[2])
@@ -53,7 +94,7 @@ $(document).ready(function(){
 					schedule[qual].push(m[3])
 				}
 			}
-		} while (m);
+		} while (m)
 		if (schedule.length == 1){
 			return ""
 		}
