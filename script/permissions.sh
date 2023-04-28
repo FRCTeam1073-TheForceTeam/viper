ab77c7daa7542b5dd07c75374127ac12012233ab
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2023-04-28 17:56:09 -0400

    No owner sticky bit

--- script/permissions.sh
+++ script/permissions.sh
@@ -6,11 +6,11 @@ if which sudo &> /dev/null
 then
 	# The web server needs to write to this directory
 	sudo chown -R `whoami`:www-data www/data/
-	sudo chmod -R g+rw www/data/
+	sudo chmod -R ug+rw www/data/
 	# Set it so that new files in that directory
 	# use the same group user and permissions
 	# as the directory itself
-	find  www/data/ -type d -exec sudo chmod ug+s {} \;
+	find  www/data/ -type d -exec sudo chmod u-s,u+x,g+sx {} \;
 
 	dir=`pwd`
 	while [ "z$dir" != "z" ]
