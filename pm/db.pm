3ffb32ee0a41bda05ecc8c277e7ff651dee6e491
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2023-05-28 19:55:20 -0400

    db connection

--- pm/db.pm
+++ pm/db.pm
@@ -15,30 +15,53 @@ sub new {
 	return $self;
 }
 
-sub getLocalConf {
-	my $file = $INC{"db.pm"};
-	$file =~ s/pm\/db\.pm/local.conf/g;
-	my $contents = read_file($file);
-	print $contents;
-	my ($host) = $contents =~ /^MYSQL_HOST\s*=\s*\"?([^\"]+)\"?/;
-	print "MYSQL HOST: $host\n"
+sub dbConnection {
+	my $configFile = $INC{"db.pm"};
+	$configFile =~ s/pm\/db\.pm/local.conf/g;
+	my $conf = read_file($configFile);
+	my($host) = $conf =~ /^MYSQL_HOST\s*=\s*\"?([^\"]+)\"?/gm;
+	my($port) = $conf =~ /^MYSQL_PORT\s*=\s*\"?([^\"]+)\"?/gm;
+	my($database) = $conf =~ /^MYSQL_DATABASE\s*=\s*\"?([^\"]+)\"?/gm;
+	my($user) = $conf =~ /^MYSQL_USER\s*=\s*\"?([^\"]+)\"?/gm;
+	my($password) = $conf	 =~ /^MYSQL_PASSWORD\s*=\s*\"?([^\"]+)\"?/gm;
+
+	return 0 if (!$host or !$port or !$database or !$user or !$password);
+
+	return DBI->connect_cached(
+		"DBI:mysql:database=$database;host=$host;port=$port",
+		$user,
+		$password,
+		{
+			RaiseError => 1,
+			PrintError => 0,
+			mysql_enable_utf8 => 1
+		}
+	);
 }
 
+sub schema {
+	my $dbh = &dbConnection();
+	return if (!$dbh);
 
-# sub dbConnection {
-# 	my $localhost = hostname();
-# 	my $dbhost = "localhost";
-# 	$dbhost = 'db001osteraws.cct1yjxl5cn2.us-west-1.rds.amazonaws.com' if ($localhost =~ /osteraws/);
-# 	return DBI->connect_cached(
-# 		"DBI:mysql:database=doozoon;host=$dbhost;port=3306",
-# 		'doozoon',
-# 		'EyjFjARzZmdm',
-# 		{
-# 			RaiseError => 1,
-# 			PrintError => 0,
-# 			mysql_enable_utf8 => 1
-# 		}
-# 	);
-# }
+	my $tableOptions = "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci";
+
+	 $dbh->do(
+		"
+			CREATE TABLE IF NOT EXISTS
+				schedule
+			(
+				owner_id VARCHAR(16),
+				event_id VARCHAR(64),
+				match_id VARCHAR(8),
+				r1 VARCHAR(8),
+				r2 VARCHAR(8),
+				r3 VARCHAR(8),
+				b1 VARCHAR(8),
+				b2 VARCHAR(8),
+				b3 VARCHAR(8)
+			)  $tableOptions
+	 	"
+	);
+}
 
 1;
