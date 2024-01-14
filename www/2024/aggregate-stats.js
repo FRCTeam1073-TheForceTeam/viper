147dacd45e15634b044b1f0729b1d7f3c2724406
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2024-01-14 10:18:54 -0500

    Aggregate stats placeholder

---
+++ www/2024/aggregate-stats.js
@@ -0,0 +1,31 @@
+"use strict"
+
+function aggregateStats(scout, aggregate){
+}
+
+var statInfo = {
+	"event": {
+		name: "Event",
+		type: "text"
+	},
+	"match": {
+		name: "Match",
+		type: "text"
+	},
+	"team": {
+		name: "Team",
+		type: "text"
+	}
+}
+
+var teamGraphs = {
+}
+
+var aggregateGraphs = {
+}
+
+function showPitScouting(el,team){
+}
+
+var whiteboardStamps = [
+]
