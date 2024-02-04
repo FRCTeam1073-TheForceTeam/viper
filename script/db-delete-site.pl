293163fef53f5f4753a166d694fa4a09f9f7528a
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2024-02-03 20:44:56 -0500

    Delete site from db

---
+++ script/db-delete-site.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use Data::Dumper;
+use lib './pm';
+use db;
+
+my $db = db->new();
+my $dbh = $db->dbConnection();
+
+my $tables = $dbh->selectall_arrayref("show tables;");
+my $site = $db->getSite();
+for my $table (@$tables){
+	$table = $table->[0];
+	$dbh->prepare("DELETE FROM `$table` WHERE `site`=?")->execute($site);
+	$db->commit();
+}
