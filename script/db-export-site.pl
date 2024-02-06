11b8a27be220598a802405f12a3c2aad2001e09c
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2024-02-06 04:08:38 -0500

    Don't write empty local files

--- script/db-export-site.pl
+++ script/db-export-site.pl
@@ -19,20 +19,26 @@ my $sth = $dbh->prepare("SELECT `local_js`, `local_css`, `background_image` FROM
 $sth->execute($db->getSite());
 my $data = $sth->fetchall_arrayref();
 if (scalar(@$data)){
-	die "Error opening $file for writing: $!" if (!open $fh, ">:encoding(UTF-8)", "$dir/local.js");
-	print $fh $data->[0]->[0];
-	close $fh;
-	die "Error opening $file for writing: $!" if (!open $fh, ">:encoding(UTF-8)", "$dir/local.css");
-	print $fh $data->[0]->[1];
-	close $fh;
-	die "Error opening $file for writing: $!" if (!open $fh, ">:raw", "$dir/local.png");
-	print $fh $data->[0]->[2];
-	close $fh;
+	if ($data->[0]->[0]){
+		die "Error opening $file for writing: $!" if (!open $fh, ">:encoding(UTF-8)", "$dir/local.js");
+		print $fh $data->[0]->[0];
+		close $fh;
+	}
+	if ($data->[0]->[1]){
+		die "Error opening $file for writing: $!" if (!open $fh, ">:encoding(UTF-8)", "$dir/local.css");
+		print $fh $data->[0]->[1];
+		close $fh;
+	}
+	if ($data->[0]->[2]){
+		die "Error opening $file for writing: $!" if (!open $fh, ">:raw", "$dir/local.png");
+		print $fh $data->[0]->[2];
+		close $fh;
+	}
 }
 
 my $events = [];
-my $sth = $dbh->prepare("SELECT `event` FROM `event` WHERE `site`=?");
-$sth->execute($db->getSite());
+my $sth = $dbh->prepare("SELECT DISTINCT `A`.`event` FROM ((SELECT DISTINCT `event` FROM `schedule` WHERE `site`=?) UNION (SELECT `event` FROM `event` WHERE `site`=?)) as `A`");
+$sth->execute($db->getSite(),$db->getSite());
 my $data = $sth->fetchall_arrayref();
 for $row (@$data){
 	my $event = $row->[0];
@@ -40,14 +46,16 @@ for $row (@$data){
 }
 
 sub queryToCsv(){
-	my ($table, $csv, $site, $event) = @_;
-	my $sth = $dbh->prepare("SELECT * FROM `$table` WHERE `site`=? and `event`=?");
+	my ($table, $csv, $site, $event, $orderby) = @_;
+	my $query = "SELECT * FROM `$table` WHERE `site`=? and `event`=? ORDER BY $orderby";
+	my $sth = $dbh->prepare($query);
 	eval {
 		$sth->execute($db->getSite(), $event);
 		1;
 	} or do {
-		print("WARNING: $@");
-		return;
+		my $err = $@;
+		return if ($err =~ /Table '.*20[0-9]{2}.*' doesn't exist/);
+		die "$err$query";
 	};
 	my $file = "$dir/$event.$csv.csv";
 	die "Error opening $file for writing: $!" if (!open $fh, ">:encoding(UTF-8)", $file);
@@ -60,26 +68,26 @@ sub queryToCsv(){
 
 for $event (@$events){
 	my ($year) = $event =~ /^(20[0-9]{2})/;
-	&queryToCsv("event", "event", $site, $event);
-	&queryToCsv("schedule", "schedule", $site, $event);
-	&queryToCsv("alliances", "alliances", $site, $event);
-	&queryToCsv("${year}scouting", "scouting", $site, $event);
-	&queryToCsv("${year}pit", "pit", $site, $event);
-	&queryToCsv("${year}subjective", "subjective", $site, $event);
+	&queryToCsv("event", "event", $site, $event, "'event'");
+	&queryToCsv("schedule", "schedule", $site, $event, "'Match'");
+	&queryToCsv("alliances", "alliances", $site, $event, "'alliance'");
+	&queryToCsv("${year}scouting", "scouting", $site, $event, "'match', 'team' + 0");
+	&queryToCsv("${year}pit", "pit", $site, $event, "'team' + 0");
+	&queryToCsv("${year}subjective", "subjective", $site, $event, "'team' + 0");
+}
 
-
-	my $sth = $dbh->prepare("SELECT `file`, `json` FROM `apijson` WHERE `site`=? and `event`=?");
-	$sth->execute($db->getSite(), $event);
-	my $data = $sth->fetchall_arrayref();
-	for $row (@$data){
-		my $file = $row->[0];
-		my $json = $row->[1];
-		my $file = "$dir/$event.$file.json";
-		print "$file\n";
-		die "Error opening $file for writing: $!" if (!open $fh, ">:encoding(UTF-8)", $file);
-		print $fh $json;
-		close $fh;
-	}
+my $sth = $dbh->prepare("SELECT `event`, `file`, `json` FROM `apijson` WHERE `site`=?");
+$sth->execute($db->getSite());
+my $data = $sth->fetchall_arrayref();
+for $row (@$data){
+	my $event = $row->[0];
+	my $file = $row->[1];
+	my $json = $row->[2];
+	my $file = "$dir/$event.$file.json";
+	print "$file\n";
+	die "Error opening $file for writing: $!" if (!open $fh, ">:encoding(UTF-8)", $file);
+	print $fh $json;
+	close $fh;
 }
 
 my $sth = $dbh->prepare("SELECT `year`, `team`, `view`, `image` FROM `images` WHERE `site`=?");
