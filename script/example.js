facb81bf3dfaa80114e1783c29747812d14b309a
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2024-02-18 06:27:18 -0500

    Add document ready

--- script/example.js
+++ script/example.js
@@ -19,15 +19,18 @@ var transferHosts = [
 // Any code here gets run on every page.
 
 // Inject a site wide message
-//$('h1').after($("<div>Hello World</div>"))
-
-// Inject a message just on the home page
-//if (location.pathname=="/") $('h1').after($(`<div>Welcome to ${ourTeam}'s scouting app!</div>`))
-
-// Change the default page title
-//$('title,h1').each(function(){
-//	$(this).text($(this).text().replace(/Viper/g,`${ourTeam}'s Scouting`))
+//$(document).ready(function(){
+//  $('h1').after($("<div>Hello World</div>"))
 //})
 
-// Use a custom CSS file
-//$('head').append("<link rel=stylesheet type=text/css href=/local.css>")
+// Inject a message just on the home page
+//$(document).ready(function(){
+//  if (location.pathname=="/") $('h1').after($(`<div>Welcome to ${ourTeam}'s scouting app!</div>`))
+//})
+
+// Change the default page title
+//$(document).ready(function(){
+//  $('title,h1').each(function(){
+//	  $(this).text($(this).text().replace(/Viper/g,`${ourTeam}'s Scouting`))
+//  })
+//})
