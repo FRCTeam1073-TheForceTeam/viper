4e190c31491213e761b96c7ba8c789eaaf4aa48d
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2024-10-11 20:34:06 -0400

    Working timeline

--- www/2024-25/scout.js
+++ www/2024-25/scout.js
@@ -6,4 +6,35 @@ $(document).ready(function(){
 			$('#to-tele-button').addClass('pulse-bg')
 		},30000)
 	})
+
+	var matchStartTime = 0
+
+	window.onShowScouting = window.onShowScouting || []
+	window.onShowScouting.push(function(){
+		matchStartTime = 0
+		return true
+	})
+
+	$('.auto label,.teleop label,.auto .count,.teleop .count').click(function(){
+		if (!matchStartTime) matchStartTime = new Date().getTime()
+		var el = $(this),
+		input = findInputInEl(findParentFromButton(el)),
+		order = $('#timeline'),
+		text = order.val(),
+		name = input.attr('name'),
+		re = name,
+		src = el.attr('src') || ""
+		if ('radio'==input.attr('type')){
+			name += `:${input.val()}`
+			text = text.replace(new RegExp(`(.*(?: |^))[0-9]+\:${re}\:[a-z0-9_]*( |$)`),"$1").trim()
+		}
+		if (/up/.test(src) || input.is(':checked')){
+			if (text) text += " "
+			var seconds = Math.round((new Date().getTime() - matchStartTime)/1000)
+			text += `${seconds}:${name}`
+		} else {
+			text = text.replace(new RegExp(`(.*(?: |^))[0-9]+\:${re}(\:[a-z0-9_]*)?( |$)`),"$1").trim()
+		}
+		order.val(text)
+	})
 })
