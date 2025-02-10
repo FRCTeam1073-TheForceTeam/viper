c711cdfba76c7a73550eb492005813d62c0244c2
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2025-02-10 07:54:49 -0500

    Show example on conversion page

--- www/import-scouting.js
+++ www/import-scouting.js
@@ -11,17 +11,27 @@ $(document).ready(function(){
 			.replace(/,/g, "،")
 	}
 	function processData(){
-		var rows = window.importFunctions[$('#format').val()].convert($('#import-data').val())
-		if (!rows || !rows.length || !rows[0].match || !rows[0].team) return
-		var headers = Object.keys(rows[0]).filter(k=>!!statInfo[k])
-		var csv = headers.join(",")+"\n"
-		rows.forEach(row=>{
-			if (row.match && row.team) csv += headers.map(k=>safeCSV(row[k])).join(",")+"\n"
-		})
+		var csv=""
+		try {
+			var rows = window.importFunctions[$('#format').val()].convert($('#import-data').val())
+			if (!rows || !rows.length || !rows[0].match || !rows[0].team) return
+			var headers = Object.keys(rows[0]).filter(k=>!!statInfo[k])
+			csv = headers.join(",")+"\n"
+			rows.forEach(row=>{
+				if (row.match && row.team) csv += headers.map(k=>safeCSV(row[k])).join(",")+"\n"
+			})
+		} catch (error){
+			console.error(error)
+		}
 		$('#csv').val(csv)
+		$('#save-container').toggle(!!csv)
+		return false
 	}
-	function setPlaceHolder(url){
-		fetch( window.importFunctions[$('#format').val()].example).then(response=>{return response.text()}).then(text=>{$('#import-data').attr('placeholder',text)})
+	function setExample(){
+		fetch( window.importFunctions[$('#format').val()].example).then(response=>{return response.text()}).then(text=>{
+			$('#import-data').attr('placeholder',text)
+			$('#example-content').text(text)
+		})
 	}
 	promiseScript(`/${eventYear}/aggregate-stats.js`).then(function(){
 		if (!window.importFunctions||!Object.keys(window.importFunctions).length){
@@ -32,9 +42,11 @@ $(document).ready(function(){
 			$('#format').append($('<option>').text(convertName).attr('value',convertName))
 		})
 		processData()
-		setPlaceHolder()
+		setExample()
 	})
-	$('#import-data,#format').on('change',processData)
-	$('#format').on('change',setPlaceHolder)
+	$('#import-data,#change').change(processData)
+	$('#format').on('change',processData)
+	$('#format').on('change',setExample)
 	$('#file').val(eventId + ".scouting.csv")
+	$('#example-button').click(function(){showLightBox($('#example-lightbox'))})
 })
