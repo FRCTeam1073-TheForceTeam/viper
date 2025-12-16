25b799b325c3d39f534cc26fb900df3ee159c7a3
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2025-12-16 17:54:30 -0500

    fix import

--- www/import-api-event.js
+++ www/import-api-event.js
@@ -159,7 +159,7 @@ $(document).ready(function(){
 				playoffMatchAlliances[round]||={}
 				csv+=matchId
 				match.teams.forEach(function(team){
-					csv+=","+(t	||0)
+					csv+=","+(team.teamNumber||0)
 					if(team.teamNumber&&teamAlliances[team.teamNumber]){
 						if(/^Red/.test(team.station))playoffMatchAlliances[matchId].red=teamAlliances[team.teamNumber]
 						if(/^Blue/.test(team.station))playoffMatchAlliances[matchId].blue=teamAlliances[team.teamNumber]
